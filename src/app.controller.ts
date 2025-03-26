df762351d1c8ecd09c001646bf716555e215d531
omkar <omakrparhad7905@gmail.com>
2025-03-26 22:11:41 +0530

    controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Post, Get, Delete, Put, Param } from '@nestjs/common';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+@Controller('book')
+export class BookController {
+  //add book
+  @Post('/add')
+  addbook(): string {
+    return 'this will add book';
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  //delete book
+  @Delete('/delete')
+  deletebook(): string {
+    return 'this will delete book';
+  }
+
+  //update book
+  @Put('/update')
+  updatebook(): string {
+    return 'this will update book';
+  }
+
+  //find all book
+  @Get('/findAll')
+  findbook(): string {
+    return 'this will find all book';
+  }
+
+  @Get('/byId/:id')
+  findOne(@Param() params: any): string {
+    console.log(params.id);
+    return `This action returns a ${params.id}`;
   }
 }

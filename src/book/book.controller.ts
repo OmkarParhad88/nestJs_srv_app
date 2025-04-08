331982ec6f1db0afbe714ab666d64fa6f06e6f0b
OmkarParhad88 <omkarparhad7905@gmail.com>
2025-04-08 12:23:02 +0530

    custom exception filter

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Get,Post, Param, ParseBoolPipe, ParseIntPipe, ValidationPipe } from "@nestjs/common";
-import { BookPipe } from "./Pipes/book.pipe";
+import { Body, Controller, Get,Post, Param, ParseBoolPipe, ParseIntPipe, ValidationPipe, UseFilters, BadRequestException } from "@nestjs/common";
 import { Book } from "./book.dto";
 import { bookException } from "./book.exception";
+import { BookCustomEXPFilter } from "./book.exception.filter";
 
 @Controller('book')
 export class BookController {
@@ -13,8 +13,15 @@ export class BookController {
   }
 
   @Get()
+  @UseFilters(BookCustomEXPFilter)
+/*************  ✨ Codeium Command ⭐  *************/
+  /**
+   * Throws a BadRequestException to test the exception filter.
+   */
+/******  9c5ac0b6-4e7f-4633-bed4-1b187a7a5acc  *******/
   helloException(): string {
-    throw new bookException();
+    throw new BadRequestException();
+    
   }
 
   @Get("/name/:id")
@@ -27,5 +34,4 @@ export class BookController {
   addBook(@Body( new ValidationPipe()) body:Book): string {
     return "Add has Book";
   }
-
 }

9e33969c652fc1c1e856829bdc874a9432b3b10a
OmkarParhad88 <omkarparhad7905@gmail.com>
2025-04-17 15:31:44 +0530

    interceptors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { BookModule } from './book/book.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [BookModule],
-  controllers: [],
+  imports: [],
+  controllers: [AppController],
   providers: [],
   exports: [],
 })
